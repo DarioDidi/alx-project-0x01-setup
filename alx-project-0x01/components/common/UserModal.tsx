43f521d1cee00e5b0884242145fb22f6403d850f
Dario Didi <dididario.dd2@gmail.com>
2025-05-30 09:22:25 +0300

    start on user modal

---
+++ alx-project-0x01/components/common/UserModal.tsx
@@ -0,0 +1,300 @@
+import { UserData, UserModalProps } from "@/interfaces";
+import React, { useState } from "react";
+
+/*export interface UserProps {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+//for User
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+//for address
+export interface Geo {
+  lat: string;
+  lng: string;
+}
+
+//for User
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}*/
+const UserModal: React.FC<UserModalProps> = ({ onClose, onSubmit }) => {
+	const [user, setUser] = useState<UserData>({
+		id: 1,
+		name: '',
+		username: '',
+		email: '',
+		address: {
+			street: '',
+			suite: '',
+			city: '',
+			zipcode: '',
+			geo: {
+				lat: '',
+				lng: '',
+			},
+		},
+		phone: '',
+		website: '',
+		company: {
+			name: '',
+			catchPhrase: '',
+			bs: '',
+		},
+	});
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+		const { name, value } = e.target;
+		setUser((prevUser) => ({ ...prevUser, [name]: value }));
+	};
+
+	const handleAddressChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target;
+		setUser((prevUser) => ({
+			...prevUser,
+			address: { ...prevUser.address, [name]: value },
+		}));
+	};
+
+	const handleGeoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target;
+		setUser((prevUser) => ({
+			...prevUser,
+			address: {
+				...prevUser.address,
+				geo: { ...prevUser.address.geo, [name]: value },
+			},
+		}));
+	};
+
+	const handleCompanyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target;
+		setUser((prevUser) => ({
+			...prevUser,
+			company: { ...prevUser.company, [name]: value },
+		}));
+	};
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		onSubmit(user);
+		onClose();
+	};
+
+	return (
+		<div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center">
+			<div className="bg-white rounded-lg p-8 shadow-lg w-full max-w-md">
+				<h2 className="text-2xl font-bold mb-4 text-gray-800">Add New Post</h2>
+				<form onSubmit={handleSubmit}>
+					<div className="mb-4">
+						<label htmlFor="userId" className="block text-gray-700 font-medium mb-2">User ID</label>
+						<input
+							type="number"
+							id="userId"
+							name="id"
+							value={user.id}
+							onChange={handleChange}
+							className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+						/>
+					</div>
+					{/*USERNAME*/}
+					<div className="mb-4">
+						<label htmlFor="username" className="block text-gray-700 font-medium mb-2">Username:</label>
+						<input
+							type="text"
+							id="username"
+							name="username"
+							value={user.username}
+							onChange={handleChange}
+							className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+							placeholder="Enter username"
+						/>
+					</div>
+					{/**/}
+					{/*name*/}
+					<div className="mb-4">
+						<label htmlFor="name" className="block text-gray-700 font-medium mb-2">Name:</label>
+						<input
+							type="text"
+							id="name"
+							name="name"
+							value={user.name}
+							onChange={handleChange}
+							className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+							placeholder="Enter name"
+						/>
+					</div>
+
+					{/*email*/}
+					<div className="mb-4">
+						<label htmlFor="email" className="block text-gray-700 font-medium mb-2">Email</label>
+						<input
+							type="email"
+							id="email"
+							name="email"
+							value={user.email}
+							onChange={handleChange}
+							className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+							placeholder="Enter email"
+						/>
+					</div>
+
+					{/*phone*/}
+					<div className="mb-4">
+						<label htmlFor="phone" className="block text-gray-700 font-medium mb-2">Phone</label>
+						<input
+							type="tel"
+							id="phone"
+							name="phone"
+							value={user.phone}
+							onChange={handleChange}
+							className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+							placeholder="Enter phone"
+						/>
+					</div>
+
+					{/*website*/}
+					<div className="mb-4">
+						<label htmlFor="website" className="block text-gray-700 font-medium mb-2">Website</label>
+						<input
+							type="url"
+							id="website"
+							name="website"
+							value={user.website}
+							onChange={handleChange}
+							className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+							placeholder="Enter website url"
+						/>
+					</div>
+					htmlFor="" className="block text-gray-700 font-medium mb-2"
+					<div className="form-section">
+						<h3>Address</h3>
+						<div className="form-group">
+							<label htmlFor="address-suite" className="block text-gray-700 font-medium mb-2">Street:</label>
+							<input
+								type="text"
+								name="street"
+								value={user.address?.street}
+								onChange={handleChange}
+								required
+							/>
+						</div>
+						<div className="form-group">
+							<label htmlFor="address-suite" className="block text-gray-700 font-medium mb-2">Suite/Apt:</label>
+							<input
+								type="text"
+								name="suite"
+								value={user.address?.suite}
+								onChange={handleChange}
+							/>
+						</div>
+						<div className="form-group">
+							<label htmlFor="city" className="block text-gray-700 font-medium mb-2">City:</label>
+							<input
+								type="text"
+								name="city"
+								value={user.address?.city}
+								onChange={handleChange}
+								required
+							/>
+						</div>
+						<div className="form-group">
+							<label htmlFor="zipcode" className="block text-gray-700 font-medium mb-2">Zip Code:</label>
+							<input
+								type="text"
+								name="zipcode"
+								value={user.address?.zipcode}
+								onChange={handleChange}
+								required
+							/>
+						</div>
+
+						<h4>Geo Location</h4>
+						<div className="form-group">
+							<label htmlFor="address-latitude" className="block text-gray-700 font-medium mb-2">Latitude:</label>
+							<input
+								type="text"
+								name="lat"
+								value={user.address?.geo.lat}
+								onChange={handleChange}
+							/>
+						</div>
+						<div className="form-group">
+							<label htmlFor="address-longitude" className="block text-gray-700 font-medium mb-2">Longitude:</label>
+							<input
+								type="text"
+								name="lng"
+								value={user.address?.geo.lng}
+								onChange={handleChange}
+							/>
+						</div>
+					</div>
+
+					<div className="form-section">
+						<h3>Company</h3>
+						<div className="form-group">
+							<label htmlFor="company-name" className="block text-gray-700 font-medium mb-2">Company Name:</label>
+							<input
+								type="text"
+								name="name"
+								value={user.company?.name}
+								onChange={handleChange}
+								required
+							/>
+						</div>
+						<div className="form-group">
+							<label htmlFor="catchphrase" className="block text-gray-700 font-medium mb-2">Catch Phrase:</label>
+							<input
+								type="text"
+								name="catchPhrase"
+								value={user.company?.catchPhrase}
+								onChange={handleChange}
+							/>
+						</div>
+						<div className="form-group">
+							<label htmlFor="business" className="block text-gray-700 font-medium mb-2">Business:</label>
+							<input
+								type="text"
+								name="bs"
+								value={user.company?.bs}
+								onChange={handleChange}
+							/>
+						</div>
+					</div>
+					{/*submit*/}
+					<div className="flex justify-between items-center">
+						<button
+							type="button"
+							onClick={onClose}
+							className="px-4 py-2 text-gray-600 hover:text-gray-800 focus:outline-none"
+						>
+							Cancel
+						</button>
+						<button
+							type="submit"
+							className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
+						>
+							Add Post
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+}
+
+export default UserModal;
